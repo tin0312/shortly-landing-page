d7c7a5d1919e0c89511d87f403f245a240f8f242
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-10-13 14:30:12 -0400

    Setup sass and tailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.css"
+import "./App.scss"
 
 function App() {
 	return (
 		<>
-			<div>Hello, landing page</div>
+			<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
 		</>
 	)
 }
