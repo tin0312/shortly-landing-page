5294635fea115421dc6e1438260e8e36870e65b8
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-10-18 17:38:04 -0400

    Reinforce desktop responsiveness & first render for statistics section

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Main from "./components/Main"
 function App() {
 	return (
 		<>
-			<div className="main-container">
+			<div className="page-container">
 				<Header />
 				<Main />
 				<Footer />
