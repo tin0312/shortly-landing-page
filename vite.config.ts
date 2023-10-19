60dae96e3f7dcc7055ce7d8587e66f73f4f93af7
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-10-19 12:52:22 -0400

    Brushing on section content & cta

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import sass from "sass"
+import url from "@rollup/plugin-url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		url({
+			limit: 10000, // inline files smaller than 10 KB as data URL strings
+			include: ["**/*.svg", "**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif"], // defaults to .svg, .png, .jpg and .jpeg files
+			emitFiles: true, // defaults to true
+		}),
+	],
 	css: {
 		preprocessorOptions: {
 			scss: {
