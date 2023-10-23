797e8118d901e298076a1d2f1549d79d910dc76a
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-10-22 23:55:46 -0400

    Setup shrtcode Developer API v2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,32 +1,52 @@
+import { useState } from "react"
 import * as Images from "../assets/images"
 
 const Main = () => {
-	// const [url, setUrl] = useState()
-	// const getUrl = (e: ChangeEvent<HTMLInputElement>) => {
-	// 	setUrl(e.target.value)
-	// }
-	// const handleSubmit = () => {
-	// 	console.log()
-	// }
+	const [url, setUrl] = useState<string | undefined>(undefined)
+	const [shortUrl, setShortUrl] = useState<string | undefined>(undefined)
+
+	const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+		e.preventDefault() // prevent the after submission navigation
+		const { value } = e.currentTarget
+		setUrl(value)
+	}
+
+	const handleSubmit = () => {
+		const apiUrl = "https://api.shrtco.de/v2/"
+		const requestUrl = `${apiUrl}/shorten?url=${url}`
+		console.log(requestUrl)
+		fetch(requestUrl)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("API Response:", data)
+				if (data.ok) {
+					setShortUrl(data.result.short_link)
+				} else {
+					console.error("API Error:", data.error)
+				}
+			})
+			.catch((error) => console.error("Fetch Error:", error))
+	}
 	return (
 		<div className="main-container">
 			<div className="section-container">
 				<div className="form-container">
-					<form>
+					<form onSubmit={handleSubmit}>
 						<input
 							type="text"
 							name="url"
 							id="url"
-							// value={url}
-							// onChange={getUrl}
+							value={url || ""}
+							onChange={handleOnChange}
 							placeholder="Shorten a link here..."
+							required
 						/>
 						<button type="submit">Shorten it!</button>
 					</form>
 				</div>
 				<div className="statistic-container">
 					<div className="statistic-header-container">
-						<h3>Advanced Statistics</h3>
+						<h3>{shortUrl}</h3>
 						<p>
 							Track how your links are performing across the web with <br />
 							our advanced statistics dashboard.
